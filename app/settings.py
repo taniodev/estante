258a5c287e95faab6deb8d7a08d04d7c9c8e79b6
Tânio Scherer <tanioms3@gmail.com>
2021-03-24 18:41:59 -0300

    Configures the Debug Toolbar
    Update dependencies

--- app/settings.py
+++ app/settings.py
@@ -75,6 +75,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'app.wsgi.application'
 
 
+# Settings for Django Debug Toolbar
+
+INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
+
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+
+
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
