5ee50f7aa3ef039773479028dc677e6fa3a32de0
Tânio Scherer <tanioms3@gmail.com>
2021-03-30 14:42:57 -0300

    Configures static files on localhost

--- app/settings.py
+++ app/settings.py
@@ -131,4 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = f'//{config("STATIC_URL")}/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+
+# Static files on another server
+remote_static_url = config('STATIC_URL')
+if remote_static_url:
+    STATIC_URL = f'//{remote_static_url}/'
