77976d37d65155183075cbd4df95c89d74281838
Tânio Scherer <tanioms3@gmail.com>
2021-03-31 15:38:53 -0300

    Adds a Bootstrap header
    Adds the Bootstrap library (version 5 beta)

--- app/base/tests/test_home.py
+++ app/base/tests/test_home.py
@@ -1,6 +1,8 @@
 import pytest
 from django.urls import reverse
 
+from app.django_assertions import assert_contains, assert_not_contains
+
 
 @pytest.fixture
 def resp(client):
@@ -9,3 +11,21 @@ def resp(client):
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_home_link_is_present(resp):
+    # In the header of all pages
+    link = reverse('base:home')
+    assert_contains(resp, f'href="{link}"')
+
+
+def test_login_link_is_present(resp):
+    # In the header of all pages
+    link = reverse('login')
+    assert_contains(resp, f'href="{link}"')
+
+
+def test_logout_link_is_not_present(resp):
+    # Appears only when the user is logged in
+    link = reverse('logout')
+    assert_not_contains(resp, f'href="{link}"')
