e0c948949a8b4caaac26b7046a90ab01f7944c7b
Tânio Scherer <tanioms3@gmail.com>
2021-06-01 13:07:55 -0300

    Only logged in users access the reading_now view

--- app/books/tests/test_reading_now_page.py
+++ app/books/tests/test_reading_now_page.py
@@ -8,4 +8,17 @@ def resp(client):
 
 
 def test_status_code(resp):
-    assert resp.status_code == 200
+    assert resp.status_code == 302
+
+
+def test_page_redirect(resp):
+    assert resp.url.startswith(reverse('login'))
+
+
+@pytest.fixture
+def resp_logged_in(client_logged_in):
+    return client_logged_in.get(reverse('books:reading_now'))
+
+
+def test_status_code_logged_in(resp_logged_in):
+    assert resp_logged_in.status_code == 200
